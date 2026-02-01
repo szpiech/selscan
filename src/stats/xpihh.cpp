598bbb3c73c63b53133e7cf30d6d98a6d7fa4549
amatur <amatur003@gmail.com>
2026-01-31 19:19:03 -0500

    xp no variation skip

--- src/stats/xpihh.cpp
+++ src/stats/xpihh.cpp
@@ -690,7 +690,7 @@ pair<double, double> XPIHH::calc_xpihh(int locus)
 {
 
     if(p.UNPHASED){
-        if(hm->hapData->get_n_c0(locus) + hm->hapData2->get_n_c0(locus) == 0  || hm->hapData->get_n_c2(locus) + hm->hapData2->get_n_c2(locus) == 0 ){  ){
+        if(hm->hapData->get_n_c0(locus) + hm->hapData2->get_n_c0(locus) == 0  || hm->hapData->get_n_c2(locus) + hm->hapData2->get_n_c2(locus) == 0 ){  
             {std::lock_guard<std::mutex> lock(mutex_log);
                 (*flog) << "WARNING: no variation. Skipping calculation at position " << hm->mapData->mapEntries[locus].physicalPos << " id: " << hm->mapData->mapEntries[locus].locusName << "\n";
             }//unlock
