97be34ae3558a461b51cd1ce2822a40316262ab4
amatur <amatur003@gmail.com>
2026-02-22 16:59:08 -0500

    bug with multiple of 32

--- src/selscan-cli.h
+++ src/selscan-cli.h
@@ -65,7 +65,7 @@ using namespace std;
 
 
 
-const string VERSION = "2.1.1";
+const string VERSION = "2.1.2";
 
 const string PREAMBLE = "\nselscan v" + VERSION + " -- a program to calculate EHH-based scans for positive selection in genomes.\n\
 Source code and binaries can be found at <https://www.github.com/szpiech/selscan>.\n\
